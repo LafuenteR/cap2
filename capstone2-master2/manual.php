72c6b7654360a38e733819fd3d8e374f2b03407a
LafuenteR <lafuente1101403@ceu.edu.ph>
2017-07-14 01:08:03 +0800

    manual

---
+++ capstone2-master2/manual.php
@@ -0,0 +1,24 @@
+PhotoSharing Website
+
+Using photo by getting or downloading it from the internet without the knowledge of the owner can lead to being sue and pay large amount of money for copyright infringement.
+
+PhotoSharing.x10host.com is about sharing your own photos to everyone for personal, school or business purposes.
+
+How it works?
+Just sign up for free. Download and Upload as much as you can.
+You can follow any users of the website.
+
+
+
+Dont's
+Do not upload photo that's not yours. You might get by real owner of the photo.
+Do not upload sexually explicit images.
+Do not upload images that contain any form of abuse.
+
+
+Admin Account
+username = lafuenter
+password = 123456
+
+Admin page can be accessed by typing photosharing.x10host.com/admin.php
+
